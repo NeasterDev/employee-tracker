88cf075806b83a347362ce9eac20a778e7025833
Nicholas Easter <nicholas.easter17@gmail.com>
2022-02-13 11:37:27 -0800

    added addToTable function and added ability to add new departments roles and employees

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const updateTable = (sql) => {
     db.query(sql, (err, data) => {
         // Error catcher
         if (err) { return console.log( err.message); }
-
-        console.log('Database successfully updated');
-
+        else {
+            console.log('Database successfully updated');
+        }
         return prompt();
     })
 }
@@ -53,18 +53,81 @@ const prompt = () => inquirer.prompt([
         generateTable(sql);
     }
     else if (answers.index === 'add a department') {
-        inquirer.prompt({
-            type:'input',
-            name: 'addDepartment',
-            message: 'Enter new department name'
-        }).then(answers => {
-            console.log(answers.addDepartment);
-            sql = `INSERT INTO departments (name) VALUES ("${answers.addDepartment}");`;
-            updateTable(sql);
-        })
-        
+        addToTable("departments");
+    }
+    else if (answers.index === 'add a role') {
+        addToTable("roles");
+    }
+    else if (answers.index === 'add an employee') {
+        addToTable("employees");
     }
 });
+
+const addToTable = (table) => {
+    let question;
+    let sql;
+    if (table === 'departments') {
+        question = {
+            type:'input',
+            name: 'departmentName',
+            message: 'Enter new department name:'
+        };
+    } else if (table === 'roles') {
+        // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+        question = [
+            {
+                type:'input',
+                name: 'roleName',
+                message: 'Enter new role name:'
+            },
+            {
+                type:'input',
+                name: 'salary',
+                message: 'Enter salary for role:'
+            },
+            {
+                type:'input',
+                name: 'departmentID',
+                message: 'Enter department id for role:'
+            }
+        ];
+        sql = ``;
+    } else if (table === 'employees') {
+        // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+        question = [
+            {
+                type:'input',
+                name: 'first_name',
+                message: 'Enter first name:'
+            },
+            {
+                type:'input',
+                name: 'last_name',
+                message: 'Enter last name:'
+            },
+            {
+                type:'input',
+                name: 'roleID',
+                message: 'Enter role id for employee:'
+            },
+            {
+                type:'input',
+                name: 'managerID',
+                message: 'Enter manager id for employee:'
+            }
+        ];
+        sql = ``;
+    }
+    inquirer.prompt(question).then(answers => {
+        //console.log(answers.addDepartment);
+        if (table === 'departments') { sql = `INSERT INTO departments (name) VALUES ("${answers.departmentName}");` }
+        else if (table === 'roles') { sql = `INSERT INTO roles (title, salary, department_id)
+                                             VALUES ("${answers.roleName}","${answers.salary}","${answers.departmentID}")`; }
+        else if (table === 'employees') { sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+                                                 VALUES ("${answers.first_name}", "${answers.last_name}", "${answers.roleID}", "${answers.managerID}")`; }
+        updateTable(sql);
+    })
+}
 
 prompt();
 
@@ -81,15 +144,15 @@ prompt();
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 
-// 4.
+// 4. (done)
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
 
-// 5.
+// 5. (done)
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
-// 6.
+// 6.(done)
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
